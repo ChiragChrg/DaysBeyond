b0f28094ced4a6f9b7dab3214a5d06e017b53324
ChiragChrg <chiruchirag2001@gmail.com>
2022-02-25 00:03:53 +0530

    Color Modifications

--- js/colors.js
+++ js/colors.js
@@ -8,7 +8,8 @@ const Per1 = document.getElementsByClassName("percent")[1];
 const Per2 = document.getElementsByClassName("percent")[2];
 const Active = document.getElementsByClassName("active")[0];
 const Side = document.getElementsByClassName("sidebar")[0];
-const Body = document.querySelector(".body");
+// const Body = document.querySelector(".body");
+const Age = document.querySelector(".age");
 const CheckAge = document.querySelector("#ageCheck");
 const FrmHd = document.querySelector("#formhead");
 const DD = document.querySelector("#DD");
@@ -25,49 +26,49 @@ console.log("Day : " + day);
 // 0 is Sunday
 
 var color1; //Top
-var color2; //Body
+var color2; //Age
 var color3; //Down
 var color4; //Per
 
 if (day == 0) {
-  color1 = "#ff1a1a"; //Red
+  color1 = "#AD0000"; //Red
   color2 = "#ff4d4d";
-  color3 = "#e60000";
+  color3 = "#990000";
   color4 = "#ff6666";
 } else if (day == 1) {
-  color1 = "#ffcc00"; //Yellow
+  color1 = "#DBAF00"; //Yellow
   color2 = "#ffdb4d";
-  color3 = "#e6b800";
+  color3 = "#cca300";
   color4 = "#ffe680";
 } else if (day == 2) {
-  color1 = "#ff3399"; //Pink
+  color1 = "#AD0057"; //Pink
   color2 = "#ff66b3";
-  color3 = "#e60073";
+  color3 = "#99004d";
   color4 = "#ff80bf";
 } else if (day == 3) {
-  color1 = "#00ff00"; //Green
+  color1 = "#009900"; //Green
   color2 = "#4dff4d";
-  color3 = "#00cc00";
+  color3 = "#008000";
   color4 = "#80ff80";
 } else if (day == 4) {
-  color1 = "#ff6600"; //Orange
+  color1 = "#C75000"; //Orange
   color2 = "#ff8533";
-  color3 = "#e65c00";
+  color3 = "#b34700";
   color4 = "#ffb380";
 } else if (day == 5) {
-  color1 = "#0066ff"; //Blue
+  color1 = "#0052CC"; //Blue
   color2 = "#4d94ff";
-  color3 = "#0052cc";
+  color3 = "#0047b3";
   color4 = "#80b3ff";
 } else if (day == 6) {
-  color1 = "#6600ff"; //Purple
+  color1 = "#6200F5"; //Purple
   color2 = "#8533ff";
-  color3 = "#5200cc";
+  color3 = "#5c00e6";
   color4 = "#b380ff";
 }
 
-Top.style.backgroundColor = color1; //Purple
-Body.style.backgroundColor = color2;
+Top.style.backgroundColor = color1;
+Age.style.backgroundColor = color2;
 Down.style.backgroundColor = color3;
 
 Year.style.backgroundColor = color1;
